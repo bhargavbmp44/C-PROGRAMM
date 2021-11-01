96514196988ca26eee5c3513419514c74e06906a
bhargavbmp44 <90056782+bhargavbmp44@users.noreply.github.com>
2021-11-01 12:28:52 +0530

    Add files via upload

---
+++ DCREASING ORDER.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void main()
+{
+	int a[100],n,i,t,j;
+	printf("enter number :");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]<a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+
+	}
+	
+
+
+	
+}
